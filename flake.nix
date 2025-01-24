45ffbf3d1fa06fe68960a28caadd00672c9067ba
Michael Kriese <michael.kriese@visualon.de>
2025-01-24 10:56:22 +0100

    minimal project

--- flake.nix
+++ flake.nix
@@ -1,5 +1,20 @@
 {
+  description = "A simple script";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.my-script;
+
+    packages.x86_64-linux.my-script =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+      pkgs.writeShellScriptBin "my-script" ''
+        DATE="$(${pkgs.ddate}/bin/ddate +'the %e of %B%, %Y')"
+        ${pkgs.cowsay}/bin/cowsay Hello, world! Today is $DATE.
+      '';
   };
 }
